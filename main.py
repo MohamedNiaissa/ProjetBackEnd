82337f416538ab0c9d8724ce722bcca708dd9265
Mohamed <mohameddrame907@gmail.com>
2022-10-10 23:52:22 +0200

    Query Parameters

--- main.py
+++ main.py
@@ -2,8 +2,41 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-file_path = "/f/f.txt"
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None, short: bool = False):
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
+#
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str): #paramètre sans valeur par défaut devient obligatoire pendant l'appem
+    item = {"item_id": item_id, "needy": needy}
+    return item
