846b80f718a34feb1e4c00d6bfb3ab779a3ebe42
Mohamed <mohameddrame907@gmail.com>
2022-10-11 10:50:08 +0200

    test exo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
+s = "" # test git add
 
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
