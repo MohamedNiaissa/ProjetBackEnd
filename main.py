2561b0eb71d311a167c5f3fb328ab49e9fba2e16
Mohamed <mohameddrame907@gmail.com>
2022-10-11 16:18:21 +0200

    exo fastAPI

--- main.py
+++ main.py
@@ -1,51 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# pas de async pour les projets
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-s = "" # test git add
-
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    """_summary_
-
-    Args:
-        skip (int, optional): _description_. Defaults to 0.
-        limit (int, optional): _description_. Defaults to 10.
-
-    Returns:
-        _type_: _description_
-    """
-    return fake_items_db[skip : skip + limit]
-
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: str, q: str | None = None, short: bool = False):
-#     item = {"item_id": item_id}
-#     if q:
-#         item.update({"q": q})
-#     if not short:
-#         item.update(
-#             {"description": "This is an amazing item that has a long description"}
-#         )
-#     return item
-#
-
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
+@app.put("/items/{item_id}")
+def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
 
 
-@app.get("/items/{item_id}")
-async def read_user_item(item_id: str, needy: str): #paramètre sans valeur par défaut devient obligatoire pendant l'appem
-    item = {"item_id": item_id, "needy": needy}
+@app.post("/items/")
+def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.post("/items/")
+def create_item(item: Item):
     return item
