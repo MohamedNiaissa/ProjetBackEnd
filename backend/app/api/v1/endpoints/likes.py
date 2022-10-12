cc70067b13884a5b99ec1b3151e58640051f8b82
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2022-10-12 12:05:01 +0200

    Rephrasing function name

--- backend/app/api/v1/endpoints/likes.py
+++ backend/app/api/v1/endpoints/likes.py
@@ -5,28 +5,28 @@ from fastapi import APIRouter, HTTPException
 router = APIRouter()
 
 @router.get("/posts/{id}")
-def get_likes_for_post_by_id(id):
+def get_likes_by_post_id(id):
     try:
         return {}
     except HTTPException:
         pass 
 
 @router.get("/comments/{id}")
-def get_likes_for_comment_by_id(id):
+def get_likes_by_comment_id(id):
     try:
         return {}
     except HTTPException:
         pass 
 
 @router.post("/comments")
-def like_comment():
+def manage_like_on_comment():
     try:
         return {}
     except HTTPException:
         pass 
 
 @router.post("/posts")
-def like_post():
+def manage_like_on_post():
     try:
         return {}
     except HTTPException:
