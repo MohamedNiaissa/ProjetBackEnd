22d31d2d024e01d1d4f89faf4059f39ecf3555d3
Mohamed <mohameddrame907@gmail.com>
2022-10-12 11:33:34 +0200

    close add endpoints

--- backend/app/api/v1/endpoints/posts.py
+++ backend/app/api/v1/endpoints/posts.py
@@ -3,3 +3,38 @@ from typing import Any, List
 from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
+
+@router.get("posts/")
+def getPosts():
+    try:
+        return {}
+    except HTTPException:
+        pass 
+
+@router.get("posts/{id}")
+def getPostById(id):
+    try:
+        return {}
+    except HTTPException:
+        pass 
+
+@router.post("posts/")
+def createPost():
+    try:
+        return {}
+    except HTTPException:
+        pass 
+
+@router.patch("posts/{id}")
+def modifyPost():
+    try:
+        return {}
+    except HTTPException:
+        pass 
+
+@router.delete("posts/{id}")
+def deletePost(id):
+    try:
+        return {}
+    except HTTPException:
+        pass 
--- backend/app/api/v1/endpoints/users.py
+++ backend/app/api/v1/endpoints/users.py
@@ -3,3 +3,45 @@ from typing import Any, List
 from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
+
+@router.get("users/")
+def getUsers():
+    try:
+        return {}
+    except HTTPException:
+        pass 
+
+@router.get("users/{id}")
+def getUserById(id):
+    try:
+        return {}
+    except HTTPException:
+        pass 
+
+@router.get("users/me")
+def getMyUser():
+    try:
+        return {}
+    except HTTPException:
+        pass 
+
+@router.post("users/")
+def createUser():
+    try:
+        return {}
+    except HTTPException:
+        pass 
+
+@router.patch("users/{id}")
+def modifyUser():
+    try:
+        return {}
+    except HTTPException:
+        pass 
+
+@router.delete("users/{id}")
+def deleteUser(id):
+    try:
+        return {}
+    except HTTPException:
+        pass 
