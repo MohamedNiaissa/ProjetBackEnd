ab857c44e72f9e277a8762ad992d153f5ab6fdaf
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-10-13 19:26:39 +0200

    close #58, fix auth guard and implements auth logic

--- backend/app/mongo/init_db.py
+++ backend/app/mongo/init_db.py
@@ -1,4 +1,9 @@
+from typing import Generator
 from pymongo import MongoClient
 from core.config import settings
 
 client = MongoClient(settings.MONGO_URI)
+
+def get_db() -> Generator:
+	db = client[settings.MONGO_DB_NAME]
+	yield db
--- backend/app/mongo/models/users.py
+++ backend/app/mongo/models/users.py
@@ -1,11 +1,24 @@
-from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+from pydantic import BaseModel, EmailStr, Field as PydanticField
+from bson import ObjectId
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
 
 
-class UserBase(BaseModel):
-    id: str = Field(alias="_id")
+class User(BaseModel):
+    id: Optional[PyObjectId] = PydanticField(default_factory=PyObjectId, alias="_id")
     username: str
     email: EmailStr
     password: str
-    token: str
-    salt: str
-    refresh_token: str
+    isAdmin: bool = False
