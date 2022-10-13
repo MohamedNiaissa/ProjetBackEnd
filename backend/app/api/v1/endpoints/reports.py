cdd50ff34ef20491cac76a8fe534e68823c7adc0
Philémon Wild <philemon.wild@gmail.com>
2022-10-13 16:35:04 +0200

    close #56 added docstrings to api/endopoints

--- backend/app/api/v1/endpoints/reports.py
+++ backend/app/api/v1/endpoints/reports.py
@@ -10,30 +10,39 @@ router = APIRouter()
 @router.get("/")
 @auth_guard("admin")
 def get_report(request: Request):
+    """
+    Retrieve reports.
+    """
     try:
         # return crud_reports.CRUD_reports.get()
 
         print(request)
         print(request.attach_user)
-        return { "sucess": True }
+        return {"success": True}
     except HTTPException:
-        pass 
-    
+        pass
+
 
 @router.post("/")
 @auth_guard("user")
 def create_report(request: Request):
+    """
+    Create new report.
+    """
     try:
         return crud_reports.CRUD_reports.create()
     except HTTPException:
-        pass 
+        pass
 
 
 @router.delete("/{id}")
 @auth_guard("admin")
 def delete_report(request: Request):
-	try:
-		# return crud_reports.CRUD_reports.delete(id)
-		print(request.attach_user)
-	except HTTPException:
-		pass 
+    """
+    Delete a specified report.
+    """
+    try:
+        # return crud_reports.CRUD_reports.delete(id)
+        print(request.attach_user)
+    except HTTPException:
+        pass
