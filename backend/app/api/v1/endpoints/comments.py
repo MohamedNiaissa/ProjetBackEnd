95de22e9a802b32fc9f9cb294c22cd65e62f29b0
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-10-14 12:04:53 +0200

    Updated the comments routes, the crud, the schemas and the models

--- backend/app/api/v1/endpoints/comments.py
+++ backend/app/api/v1/endpoints/comments.py
@@ -1,56 +1,78 @@
 from typing import Any, List
 
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request, status, Body
 from api.deps import auth_guard
-from crud import crud_comments
+from crud.crud_comments import comments
+from mongo.schemas.comments import CommentCreate, CommentToUpdate
+from mongo.models.comments import Comment, CommentUpdate
 
 router = APIRouter()
 
 
 @router.get("/")
 def get_comments():
-    """
-	Retrieve comments.
-    """
-    try:
-        return crud_comments.CRUD_comments.get_all()
-    except HTTPException:
-        pass
+	"""_summary_
+
+	Raises:
+		HTTPException: _description_
+
+	Returns:
+		_type_: _description_
+	"""
+	comments_list = comments.get_all()
+	if len(comments_list) is 0:
+		raise HTTPException(
+			status_code=status.HTTP_404_NOT_FOUND,
+			detail="No comments were found",
+		)
+	return comments_list
 
 
 @router.post("/")
 @auth_guard("user")
-def create_comment(request: Request):
-    """
-    Create a comment.
-    """
-    try:
-        return crud_comments.CRUD_comments.create()
-    except HTTPException:
-        pass
+def create_comment(request: Request, comment_create: CommentCreate = Body(...)):
+	"""_summary_
+
+	Args:
+		request (Request): _description_
+		comment_create (CommentCreate, optional): _description_. Defaults to Body(...).
+
+	Returns:
+		_type_: _description_
+	"""
+	comment_data = Comment.assert_model(request.attach_user["id"], comment_create)
+	id = comments.create(comment_data)
+	return { "status": "OK", "comment_id": id }
 
 
 @router.patch("/{id}")
 @auth_guard("user")
-def modify_comment(request: Request):
-    """
-    Update a comment.
-    """
-    try:
-        # return crud_comments.CRUD_comments.modify(id)
-        print(request.attach_user)
-    except HTTPException:
-        pass
+def update_comment(request: Request, id: str, comment_update: CommentToUpdate = Body(...)):
+	"""_summary_
+
+	Args:
+		request (Request): _description_
+		id (str): _description_
+		comment_update (CommentToUpdate, optional): _description_. Defaults to Body(...).
+
+	Returns:
+		_type_: _description_
+	"""
+	comment = CommentUpdate.assert_model_id(id, comment_update)
+	comments.update(comment)
+	return { "status": "OK" }
 
 
 @router.delete("/{id}")
 @auth_guard("user")
-def delete_comment(request: Request):
-    """
-    Delete a comment.
-    """
-    try:
-        # return crud_comments.CRUD_comments.delete(id)
-        print(request.attach_user)
-    except HTTPException:
-        pass
+def delete_comment(request: Request, id: str):
+	"""_summary_
+
+	Args:
+		request (Request): _description_
+
+	Returns:
+		_type_: _description_
+	"""
+	comments.delete(id)
+	return { "status": "OK" }
--- backend/app/mongo/models/comments.py
+++ backend/app/mongo/models/comments.py
@@ -1,25 +1,52 @@
+from typing_extensions import Self
 from pydantic import BaseModel, Field
 
+from mongo.schemas.comments import CommentCreate, CommentToUpdate
 
-class CommentBase(BaseModel):
-    id: str = Field(alias="_id")
-    message: str
-    id_user: str = Field(alias="userId")
-    id_post: str = Field(alias="postId")
-    nb_like: int
 
-    # might need to tag id_user
-    # #and id_post more appropriately
-    # #since they are supposed
-    # to be ObjectIds
+class Comment(BaseModel):
+	message: str
+	userId: str
+	postId: str
+	
+	@classmethod
+	def assert_model(self, user_id: str, comment_data: CommentCreate) -> Self:
+		return self(
+			userId=user_id,
+			message=comment_data.message,
+			postId=comment_data.post_id,
+		)
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "id": 758,
-                "message": "Nice Post you got there",
-                "id_user": 30,
-                "id_post": 304,
-                "nb_like": 0
-            }
-        }
+	class Config:
+		schema_extra = {
+			"example": {
+				"id": 758,
+				"message": "Nice Post you got there",
+				"id_user": 30,
+				"id_post": 304,
+				"nb_like": 0
+			}
+		}
+
+
+class CommentUpdate(BaseModel):
+	id: str
+	message: str
+	
+	@classmethod
+	def assert_model_id(self, id: str, comment_data: CommentToUpdate) -> Self:
+		return self(
+			id=id,
+			message=comment_data.message,
+		)
+
+	class Config:
+		schema_extra = {
+			"example": {
+				"id": 758,
+				"message": "Nice Post you got there",
+				"id_user": 30,
+				"id_post": 304,
+				"nb_like": 0
+			}
+		}
