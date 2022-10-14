ef08a38b3b02281d305501740136c389fa42c527
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-10-14 12:03:22 +0200

    Updated the reports routes, the crud, schemas and models

--- backend/app/mongo/models/reports.py
+++ backend/app/mongo/models/reports.py
@@ -1,20 +1,30 @@
+from typing_extensions import Self
 from pydantic import BaseModel, Field
+from mongo.schemas.reports import ReportBase
 
 
-class ReportBase(BaseModel):
-    id: str = Field(alias="_id")
-    user_id: str = Field(alias="userId")
-    post_id: str | None = Field(alias="postId")
-    comment_id: str | None = Field(alias="commentId")
-    reason: int
+class Report(BaseModel):
+	reason: int
+	userId: str
+	target: str
+	targetId: str
+	
+	@classmethod
+	def assert_model(self, report_data: ReportBase) -> Self:
+		return self(
+			reason=report_data.reason,
+			userId=report_data.user_id,
+			target=report_data.target,
+			targetId=report_data.target_id,
+		)
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "id": 12,
-                "user_id": 930,
-                "post_id": 3984,
-                "comment_id": 22,
-                "reason": 1
-            }
-        }
+	class Config:
+		schema_extra = {
+			"example": {
+				"id": 12,
+				"user_id": 930,
+				"post_id": 3984,
+				"comment_id": 22,
+				"reason": 1
+			}
+		}
