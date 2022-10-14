fdc00c1cd4908506580c5adf710d81eec2c6438e
Philémon Wild <philemon.wild@gmail.com>
2022-10-14 12:49:50 +0200

    close #70 added docstrings to endpoints/likes

--- backend/app/api/v1/endpoints/likes.py
+++ backend/app/api/v1/endpoints/likes.py
@@ -10,7 +10,16 @@ router = APIRouter()
 @router.get("/posts/{id}")
 def get_likes_by_post_id():
     """
-    Retrieve a specified like.
+    Gets like status from a specified post
+
+    Args:
+        string (id): id of the post to get status from
+
+    Raises:
+        HTTPException: raise 404 if no likes were found or if no posts were found
+
+    Returns:
+        JSON : infos of that like
     """
     try:
         # return crud_likes.CRUD_likes.get_by_comment_id(id)
@@ -22,7 +31,16 @@ def get_likes_by_post_id():
 @router.get("/comments/{id}")
 def get_likes_by_comment_id():
     """
-    Retrieve a like by using a specified comment.
+    Gets like status from a specified comment
+
+    Args:
+        string (id): id of the comment to get status from
+
+    Raises:
+        HTTPException: raise 404 if no likes were found or if no comments were found
+
+    Returns:
+        JSON : infos of that like
     """
     try:
         # return crud_likes.CRUD_likes.get_by_comment_id(id)
@@ -35,7 +53,16 @@ def get_likes_by_comment_id():
 @auth_guard("user")
 def manage_like_on_comment(request: Request):
     """
-    Create a like on a comment.
+    Changes the status of the like and infos linked to the comment such as the commentID and userID
+
+    Args:
+        request (Request): comment details are linked to it
+
+    Raises:
+        HTTPException: raise 404 if no comment is found
+
+    Returns:
+        JSON: all the infos of that like
     """
     try:
         return crud_likes.CRUD_likes.manage_on_comment()
@@ -47,7 +74,16 @@ def manage_like_on_comment(request: Request):
 @auth_guard("user")
 def manage_like_on_post(request: Request):
     """
-    Create a like on a post.
+    Changes the status of the like and infos linked to the comment such as the commentID and userID
+
+    Args:
+        request (Request): comment details are linked to it
+
+    Raises:
+        HTTPException: raise 404 if no comment is found
+
+    Returns:
+        JSON: all the infos of that like
     """
     try:
         return crud_likes.CRUD_likes.manage_on_post()
